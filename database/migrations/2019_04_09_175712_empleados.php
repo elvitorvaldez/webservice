988bc1e09be8550af26e2ef832d5b6fec89da667
elvitorvaldez <elvitorvaldez@gmail.com>
2019-04-09 18:18:27 -0500

    mi primer commit

--- database/migrations/2019_04_09_175712_empleados.php
+++ database/migrations/2019_04_09_175712_empleados.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Alumnos extends Migration
+class Empleados extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,18 @@ class Alumnos extends Migration
      */
     public function up()
     {
-        Schema::table('t_alumnos', function (Blueprint $table) {
+        Schema::table('t_empleados', function (Blueprint $table) {
             $table->increments('id_t_usuarios');
             
             $table->string('nombre');
-            $table->string('ap_paterno');
-            $table->string('ap_materno');            
-            $table->int('activo');
+            $table->string('email');
+            $table->string('puesto'); 
+            $table->date('fecha_nac');
+            $table->string('domicilio');
+            $table->string('skill1');            
+            $table->int('cal1');
+            $table->string('skill2');            
+            $table->int('cal2');
         });
     }
 
@@ -30,7 +35,7 @@ class Alumnos extends Migration
      */
     public function down()
     {
-        Schema::table('t_alumnos', function (Blueprint $table) {
+        Schema::table('t_empleados', function (Blueprint $table) {
             //
         });
     }
